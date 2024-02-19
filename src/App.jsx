1e2090635991daca3a51a93a01d0ba7c01ed3c94
Priyanka Kharel <priyankakharel282@gmail.com>
2024-02-19 17:01:33 +0545

    Project updated upto clicking X or O alternatively

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Square from "./components/Square";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
-      <div className="m-0">
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 w-16 p-5 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-      </div>
-      <div className="m-0">
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-      </div>
-      <div>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-        <input
-          type="button"
-          value="X"
-          className="m-0.5 p-5 w-16 bg-white"
-        ></input>
-      </div>
+      <>
+        <div className="status"></div>
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </>
     </>
   );
 }
